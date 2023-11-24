ac66f7ed9a0dd40afebc4a3b6d1a4eb228aae93f
crossfire3001 <obiwan2035@mail.ru>
2023-11-24 18:42:19 +0500

    done my first modal for desktop

--- js/form.js
+++ js/form.js
@@ -1,44 +1,44 @@
 let loader = $(".loader");
 let reservationForm = $(".reservation__form");
-let orderTitle = $(".order__text-title");
 let orderMessage = $(".order__text-message");
+let modal = $(".modal__bg");
 
 $("#submit").click(function (e) {
-    e.preventDefault();
-    let name = $("#name");
-    let phone = $("#phone");
-    let hasError = false;
+  e.preventDefault();
+  let name = $("#name");
+  let phone = $("#phone");
+  let hasError = false;
 
-    $(".error-input").hide();
+  $(".error-input").hide();
 
-    if (!name.val()) {
-        name.next().show();
-        hasError = true;
-    }
-    if (!phone.val()) {
-        phone.next().show();
-        hasError = true;
-    }
+  if (!name.val()) {
+    name.next().show();
+    hasError = true;
+  }
+  if (!phone.val()) {
+    phone.next().show();
+    hasError = true;
+  }
 
-    if (!hasError) {
-        $.ajax({
-            method: "POST",
-            url: "https://testologia.site/checkout",
-            data: { name: name.val(), phone: phone.val() },
-        }).done(function (msg) {
-            loader.hide();
-            if (!msg.success) {
-                loader.css("display", "flex");
-                setTimeout(function () {
-                    loader.hide();
-                    alert(
-                        "Возникла ошибка при оформлении заказа, позвоните нам и сделайте заказ"
-                    );
-                }, 3000);
-            }
-            reservationForm.hide();
-            orderTitle.text("Спасибо за Ваш заказ. Мы скоро свяжемся с Вами!");
-            orderMessage.hide();
-        });
-    }
+  if (!hasError) {
+    $.ajax({
+      method: "POST",
+      url: "https://testologia.site/checkout",
+      data: { name: name.val(), phone: phone.val() },
+    }).done(function (msg) {
+      loader.hide();
+      if (!msg.success) {
+        loader.css("display", "flex");
+        setTimeout(function () {
+          loader.hide();
+          alert(
+            "Возникла ошибка при оформлении заказа, позвоните нам и сделайте заказ"
+          );
+        }, 3000);
+      }
+      reservationForm.hide();
+      modal.css("display", "flex");
+      orderMessage.hide();
+    });
+  }
 });
