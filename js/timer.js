eb5ef40170f33399455ad8e5dfc78f44a6d69e1c
crossfire3001 <obiwan2035@mail.ru>
2023-11-27 18:50:50 +0500

    timer now working on JQuery

--- js/timer.js
+++ js/timer.js
@@ -3,27 +3,26 @@ const countdownTime = 30 * 60;
 function startCountdown() {
     let seconds = countdownTime;
 
-    const minutesElement = document.getElementById("minutes");
-    const secondsElement = document.getElementById("seconds");
-
-    const minutesTens = document.querySelector("[data-minutes-tens]");
-    const minutesOnes = document.querySelector("[data-minutes-ones]");
-    const secondsTens = document.querySelector("[data-seconds-tens]");
-    const secondsOnes = document.querySelector("[data-seconds-ones]");
+    const minutesElement = $(".minutes");
+    const secondsElement = $(".seconds");
 
     function updateTimer() {
         const minutes = Math.floor(seconds / 60);
         const remainingSeconds = seconds % 60;
 
-        minutesElement.textContent = String(minutes).padStart(2, "0");
-        secondsElement.textContent = String(remainingSeconds).padStart(2, "0");
+        minutesElement[0].textContent = String(minutes).slice(0, 1);
+        minutesElement[1].textContent = String(minutes).slice(1);
+        secondsElement[0].textContent = String(remainingSeconds).slice(0, 1);
+        secondsElement[1].textContent = String(remainingSeconds).slice(1);
 
         seconds--;
 
         if (seconds < 0) {
             clearInterval(interval);
-            minutesElement.textContent = "00";
-            secondsElement.textContent = "00";
+            minutesElement[0].textContent = "0";
+            minutesElement[1].textContent = "0";
+            secondsElement[0].textContent = "0";
+            secondsElement[1].textContent = "0";
         }
     }
 
@@ -33,6 +32,3 @@ function startCountdown() {
 }
 
 document.addEventListener("DOMContentLoaded", startCountdown);
-
-const test = new Date().setMinutes(new Date().getMinutes() + 30);
-// console.log(test);
