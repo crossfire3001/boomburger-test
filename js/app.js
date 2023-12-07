0abeb769132fb41fbc572502c64edadc34c7a4c5
crossfire3001 <obiwan2035@mail.ru>
2023-12-07 10:25:16 +0500

    jQuery slick minor fixes

--- js/app.js
+++ js/app.js
@@ -1,68 +1,70 @@
-$(".carousel").slick({
-  rows: 1,
-  slidesPerRow: 4,
-  prevArrow:
-    '<button type="button" class="slick-prev"><img src="img/menu__fork.png" alt="fork"></button>',
-  nextArrow:
-    '<button type="button" class="slick-next"><img src="img/menu__knife.png" alt="knife"></button>',
-  responsive: [
-    {
-      breakpoint: 576,
-      settings: {
-        slidesPerRow: 2,
-        rows: 2,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesPerRow: 2,
-        rows: 1,
-      },
-    },
-    {
-      breakpoint: 992,
-      settings: {
-        slidesPerRow: 2,
-        rows: 1,
-      },
-    },
-  ],
-});
-
-$(".feedback__carousel").slick({
-  rows: 1,
-  slidesPerRow: 3,
-  prevArrow:
-    '<button type="button" class="slick-prev"><img src="img/menu__fork.png" alt="fork"></button>', // Custom HTML for the previous button
-  nextArrow:
-    '<button type="button" class="slick-next"><img src="img/menu__knife.png" alt="knife"></button>', // Custom HTML for the next button
-  responsive: [
-    {
-      breakpoint: 576,
-      settings: {
-        slidesPerRow: 1,
-        rows: 2,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesPerRow: 2,
-        rows: 1,
-      },
-    },
-    {
-      breakpoint: 992,
-      settings: {
-        slidesPerRow: 2,
-        rows: 1,
-      },
-    },
-  ],
-});
-
 $(document).ready(function () {
+
+  $(".carousel").slick({
+    rows: 1,
+    slidesPerRow: 4,
+    prevArrow:
+        '<button type="button" class="slick-prev"><img src="img/menu__fork.png" alt="fork"></button>',
+    nextArrow:
+        '<button type="button" class="slick-next"><img src="img/menu__knife.png" alt="knife"></button>',
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesPerRow: 2,
+          rows: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesPerRow: 2,
+          rows: 1,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesPerRow: 2,
+          rows: 1,
+        },
+      },
+    ],
+  });
+
+  $(".feedback__carousel").slick({
+    rows: 1,
+    slidesPerRow: 3,
+    prevArrow:
+        '<button type="button" class="slick-prev"><img src="img/menu__fork.png" alt="fork"></button>', // Custom HTML for the previous button
+    nextArrow:
+        '<button type="button" class="slick-next"><img src="img/menu__knife.png" alt="knife"></button>', // Custom HTML for the next button
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesPerRow: 1,
+          rows: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesPerRow: 2,
+          rows: 1,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesPerRow: 2,
+          rows: 1,
+        },
+      },
+    ],
+  });
+
+
   $('.slider__menu').slick({
     slidesToShow: 3, // сколько показывать слайдов
     responsive: [
